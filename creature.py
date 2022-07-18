7ec92c0b2cddcadda16ae93caeb9eb6632d636b5
Daniil Shupikov <dashpkv@gmail.com>
2022-07-19 01:36:26 +0300

    added Creature class, new Enemy class and Hero class derive from Creature

--- creature.py
+++ creature.py
@@ -1,38 +1,71 @@
+from abc import ABC, abstractmethod
+
 from inventory import Inventory, InventoryIsFullException
 from items import Key
+from log import Log
 from readchar import readkey
 from globals import DROP_KEYS
 from debug_log import debug
 
 
-class Hero:
-    id = 0
-    tile_char = '☻'
+LOG = Log(log_line=12)
 
-    def __init__(self, x_position, y_position, name, level, log):
-        self.x_pos = x_position
-        self.y_pos = y_position
+
+class Creature(ABC):
+    def __init__(self, name, x_pos, y_pos, level):
         self.name = name
+        self.x_pos = x_pos
+        self.y_pos = y_pos
         self.level = level
-        self.inventory = Inventory(inv_col=self.level.get_size('width') + 1, height=self.level.get_size('height'))
-        self.log = log
+        self.can_walk_on = False
 
     def move(self, x, y):
         try:
-            if self.level.get_object(x, y).can_walk_on:
+            if self.level.check_for_move(x, y):
                 self.level.upd_chars.append((x, y))
                 self.level.upd_chars.append((self.x_pos, self.y_pos))
-                self.level.place_object(self, x, y)  # TODO: change to move object
-                self.level.remove_object(self, self.x_pos, self.y_pos)
+                self.level.move_object(self, x, y)
+                self.x_pos = x
+                self.y_pos = y
+        except IndexError:
+            pass
+
+
+class Enemy(Creature):
+    def __init__(self, x_position, y_position, name, level):
+        super().__init__(name, x_position, y_position, level)
+        self.inventory = Inventory(inv_col=self.level.get_size('width') + 1, height=self.level.get_size('height'))
+        self.tile_char = 'z'
+        self.id = 4
+        self.name_a = 'a zombie'
+
+
+class Hero(Creature):
+    id = 0
+    tile_char = '☻'
+
+    def __init__(self, x_position, y_position, name, level):
+        super().__init__(name, x_position, y_position, level)
+        self.inventory = Inventory(
+            inv_col=self.level.get_size('width') + 1,
+            height=self.level.get_size('height'),
+            visible=True)
+
+    def move(self, x, y):
+        try:
+            if self.level.check_for_move(x, y):
+                self.level.upd_chars.append((x, y))
+                self.level.upd_chars.append((self.x_pos, self.y_pos))
+                self.level.move_object(self, x, y)
                 self.x_pos = x
                 self.y_pos = y
             else:
                 try:
                     self.open(x, y)
                 except AttributeError:
-                    self.log.add_msg("You can't move here.")
+                    LOG.add_msg("You can't move here.")
         except IndexError:
-            self.log.add_msg("You can't move here.")
+            LOG.add_msg("You can't move here.")
 
     def open(self, x, y):
         obj_name = self.level.get_object(x, y).name_a
@@ -42,11 +75,11 @@ class Hero:
                 self.level.upd_chars.append((self.x_pos, self.y_pos))
                 self.level.get_object(x, y).open()
                 self.inventory.remove_item(Key.id)
-                self.log.add_msg(f'You opened {obj_name}.')
+                LOG.add_msg(f'You opened {obj_name}.')
             else:
-                self.log.add_msg(f'You need to find a key to open {obj_name}.')
+                LOG.add_msg(f'You need to find a key to open {obj_name}.')
         else:
-            self.log.add_msg(f"You can't open {obj_name}.")
+            LOG.add_msg(f"You can't open {obj_name}.")
 
     def grab(self):
         objects_below = self.level.get_object(self.x_pos, self.y_pos).objects_on
@@ -58,46 +91,46 @@ class Hero:
                 try:
                     self.inventory.add_item(objects_below[i])
                 except InventoryIsFullException:
-                    self.log.add_msg("Can't take any more items. Inventory is full.")
+                    LOG.add_msg("Can't take any more items. Inventory is full.")
                     return False
                 obj_name = objects_below[i].name_a
                 self.level.remove_object(objects_below[i], self.x_pos, self.y_pos)
                 self.level.upd_chars.append((self.x_pos, self.y_pos))
-                self.log.add_msg(f'You grabbed {obj_name} from {tile_name}.')
+                LOG.add_msg(f'You grabbed {obj_name} from {tile_name}.')
                 return True
         else:
-            self.log.add_msg('There are no items here.')
+            LOG.add_msg('There are no items here.')
 
     def drop(self):
         if len(self.inventory.content) == 0:
-            self.log.add_msg('You have no items in your backpack.')
+            LOG.add_msg('You have no items in your backpack.')
             return False
-        self.log.add_msg('Select an item to drop. Press C to cancel')
+        LOG.add_msg('Select an item to drop. Press C to cancel')
         while True:
             try:
                 key_pressed = readkey()
                 if key_pressed == 'c':
-                    self.log.add_msg('You changed your mind on dropping something.')
+                    LOG.add_msg('You changed your mind on dropping something.')
                     return False
                 if key_pressed not in DROP_KEYS:
-                    self.log.add_msg('Select a proper item in your backpack.')
+                    LOG.add_msg('Select a proper item in your backpack.')
                 else:
                     item_index = DROP_KEYS.index(key_pressed)
                     if item_index >= len(self.inventory.content):
-                        self.log.add_msg('Select a proper item in your backpack.')
+                        LOG.add_msg('Select a proper item in your backpack.')
                     else:
                         self.level.place_object(self.inventory.content[item_index], self.x_pos, self.y_pos)
-                        self.log.add_msg(f'You dropped {self.inventory.content[item_index].name_a}.')
+                        LOG.add_msg(f'You dropped {self.inventory.content[item_index].name_a}.')
                         del self.inventory.content[item_index]
                         self.inventory.draw()
                         return True
             except UnicodeDecodeError:
-                self.log.add_msg('Try switching to ENG layout')
+                LOG.add_msg('Try switching to ENG layout')
 
     def look(self):
         objects_below = self.level.get_object(self.x_pos, self.y_pos).objects_on
         tile_below = self.level.get_object(self.x_pos, self.y_pos)
         if len(objects_below) > 1:
-            self.log.add_msg(f'You see {objects_below[-2].description[:1].lower()}{objects_below[-2].description[1:]}')
+            LOG.add_msg(f'You see {objects_below[-2].description[:1].lower()}{objects_below[-2].description[1:]}')
         else:
-            self.log.add_msg(f'You see {tile_below.description[:1].lower()}{tile_below.description[1:]}')
+            LOG.add_msg(f'You see {tile_below.description[:1].lower()}{tile_below.description[1:]}')
